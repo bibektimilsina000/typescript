96d7a6b5717d52295bfd5066803170c3da4476d7
cicero <bibektimilsina000@gmail.com>
2022-08-16 19:06:47 +0545

    app

--- apps.js
+++ apps.js
@@ -1,2 +1,11 @@
-var num = 10;
-console.warn(num);
+// 01
+// const Ten: number = 10
+// console.warn(Ten)
+var num = 12;
+var str = "abc";
+var isTrue = false;
+function add(n1, n2) {
+    return n1 + n2;
+}
+var result = add(10, 20);
+console.warn(result);
--- apps.ts
+++ apps.ts
@@ -1,3 +1,19 @@
-const Ten: number = 10
+// 01
 
-console.warn(Ten)
+// const Ten: number = 10
+
+// console.warn(Ten)
+
+let num: number = 12;
+let str: string = "abc";
+
+let isTrue: boolean = false
+
+
+function add(n1: number, n2: number) {
+    return n1 + n2
+}
+
+const result = add(10, 20)
+console.warn(result)
+
